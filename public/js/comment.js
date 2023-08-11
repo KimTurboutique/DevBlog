6fc893f71da1ccc9f2133d060089087065e23fdf
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-08-11 18:10:28 -0400

    updated comment.js, linked to single post handlebar

---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  const post_id = document.querySelector('input').value
+
+    const comment = document.querySelector('#project-desc').value.trim();
+
+    if (comment) {
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ comment, post_id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to create comment');
+      }
+    }
+  };
+
+document.querySelector('.new-project-form').addEventListener ('submit', newFormHandler)  
