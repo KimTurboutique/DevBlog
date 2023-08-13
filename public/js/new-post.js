4207a137f36fec7886ab7af9f3c55ebf9d7f80ae
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-08-12 20:10:02 -0400

    added update and delete btn, unable to debug error and get btns to function

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -1,24 +1,45 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
   const title = document.querySelector('input').value.trim()
 
-    const content = document.querySelector('#project-desc').value.trim();
+  const content = document.querySelector('#project-desc').value.trim();
 
-    if (title && content) {
-      const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({ title, content }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to create post');
-      }
+  if (title && content) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create post');
     }
-  };
+  }
+};
 
-document.querySelector('.new-project-form').addEventListener ('submit', newFormHandler)  
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('post-id')) {
+    const id = event.target.getAttribute('post-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-project-form').addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.new-project-form')
+  .addEventListener('click', delButtonHandler);
